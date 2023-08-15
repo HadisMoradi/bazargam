c520b2455a085887aed1ce813056c52e6e7f9843
Hadis <1hadis3.8moradi3@gmail.com>
2023-08-15 10:07:16 +0330

    ordered

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
+
+import Products from '../components/Products.vue'
+import Call from '../components/Call.vue'
+
 </script>
 
 <template>
-  <main>
-    <!-- <TheWelcome /> -->
+  <main> 
+     <div class="image-back">
+
+      <img  src="../../public/noroz.png" alt="">
+      <img src="../../public/ghest.png" alt="">
+    
+    </div> 
   </main>
+   
+  <Products/>
 </template>
+
+<style>
+.image-back{
+  padding: 10px 170px;
+  border-radius: 10px;
+  margin: 10px;
+}
+</style>
